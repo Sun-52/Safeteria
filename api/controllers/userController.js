aff5fec05e0bcab1950a54f52bd0ee036586632f
Sunny <iamsun2552@gmail.com>
2022-11-16 11:11:34 +0700

    Change login system

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,37 +1,52 @@
 const mongoose = require("mongoose");
 const user = mongoose.model("user");
 
-exports.sign_in = async (req, res) => {
-  let userInfoResponse = await fetch(
-    "https://www.googleapis.com/userinfo/v2/me",
-    {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    }
-  );
+// exports.sign_in = async (req, res) => {
+//   let userInfoResponse = await fetch(
+//     "https://www.googleapis.com/userinfo/v2/me",
+//     {
+//       headers: {
+//         Authorization: `Bearer ${accessToken}`,
+//       },
+//     }
+//   );
 
-  let _userInfo = await userInfoResponse.json();
-  // TODO use token and check in server
-  _userInfo = {
-    name: _userInfo.name,
-    email: _userInfo.email,
-    picture: _userInfo.picture,
-    googleId: _userInfo.id,
-    provider: "google",
-  };
-  user.findOne({ googleId: _userInfo.googleId }, (err, user) => {
-    if (err) {
-      res.send(err);
-    } else if (null) {
-      newUser = new user(_userInfo);
-      newUser.save((err, user) => {
-        if (err) res.send(err);
-        res.json(user);
-      });
-    } else {
-      res.send(_userInfo);
-    }
+//   let _userInfo = await userInfoResponse.json();
+//   // TODO use token and check in server
+//   _userInfo = {
+//     name: _userInfo.name,
+//     email: _userInfo.email,
+//     picture: _userInfo.picture,
+//     googleId: _userInfo.id,
+//     provider: "google",
+//   };
+//   user.findOne({ googleId: _userInfo.googleId }, (err, user) => {
+//     if (err) {
+//       res.send(err);
+//     } else if (null) {
+//       newUser = new user(_userInfo);
+//       newUser.save((err, user) => {
+//         if (err) res.send(err);
+//         res.json(user);
+//       });
+//     } else {
+//       res.send(_userInfo);
+//     }
+//   });
+// };
+
+exports.sign_up = (req, res) => {
+  const newUser = new user(req.body);
+  newUser.save((err, user) => {
+    if (err) res.send(err);
+    res.json(user);
+  });
+};
+
+exports.sign_in = (req, res) => {
+  user.findById(req.params.user_id, (err, user) => {
+    if (err) res.send(err);
+    res.json(user);
   });
 };
 
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const taskBuilder = require("../controllers/userController.js");
 
 module.exports = (app) => {
-  app.route("/user").post(taskBuilder.sign_in);
-  app.route("/user/:user_id").patch(taskBuilder.change_role);
+  app.route("/user").post(taskBuilder.sign_up);
+  app.route("/user/:user_id").get(taskBuilder.sign_in).patch(taskBuilder.change_role);
 };
