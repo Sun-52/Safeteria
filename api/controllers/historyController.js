cb0cc525cbf7a2ec3cf998486480f9c719f13d96
Sunny <iamsun2552@gmail.com>
2022-11-20 12:57:42 +0700

    Change method

--- api/controllers/historyController.js
+++ api/controllers/historyController.js
@@ -65,16 +65,16 @@ exports.generate_qr = async (req, res) => {
   });
 };
 
-exports.gen_que = (req, res) => {
-  const random = Math.floor(Math.random() * 9000 + 1000);
-  order.findByIdAndUpdate(
-    req.params.order_id,
-    { que: random },
-    (err, order) => {
-      if (err) res.send(err);
-      res.json(order);
-    }
-  );
+exports.gen_que = async (req, res) => {
+  try {
+    const random = Math.floor(Math.random() * 9000 + 1000);
+    const user_order = await order.findById(req.params.order_id);
+    user_order.que = random;
+    await user_order.save();
+    res.json(user_order);
+  } catch (err) {
+    res.send(err);
+  }
 };
 
 exports.get_que = (req, res) => {
