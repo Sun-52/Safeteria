10bd49e0f1e0f4fe3eeb622819b329a0a482e2a5
Sunny <iamsun2552@gmail.com>
2022-12-06 20:12:49 +0700

    Fix typo

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -6,7 +6,7 @@ module.exports = (app) => {
     .route("/user/:user_id")
     .get(taskBuilder.sign_in)
     .patch(taskBuilder.change_role);
-  app.route("/user/monet/increase").patch(taskBuilder.increase_money);
-  app.route("/user/monet/decrease").patch(taskBuilder.decrease_money);
+  app.route("/user/money/increase").patch(taskBuilder.increase_money);
+  app.route("/user/money/decrease").patch(taskBuilder.decrease_money);
   app.route("/user/pay/:user_id/:order:id").patch(taskBuilder.pay);
 };
